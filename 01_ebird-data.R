b3e6ff25478cab81cc39b069f234c45686f4c0e0
Alison Johnston <ali@dhcp-clara-4168.eduroam.cornell.edu>
2019-08-09 12:42:07 -0400

    Changes to ebird data extraction
    now collecting data for
    2017-2018
    may and june
    
    other params as before.

--- 01_ebird-data.R
+++ 01_ebird-data.R
@@ -12,21 +12,35 @@ library(readr)
 # custom functions
 walk(list.files("R", full.names = TRUE), source)
 
+# location of large unzipped eBird data
+ebd_data_folder <- "/Volumes/ebird_data/EBD/"
+ebd_release <- "May-2019"
+
+# where to save the processed datasets
+ebd_save <- "data/"
+
+# name for this specific data extraction
+data_tag <- "mayjune_201718_bcr27"
 
 # zero-filled ebird data ----
 
 # ebd extraction
-f_ebd <- "data/ebd_june_bcr27.txt"
-f_sampling <- "data/ebd_june_bcr27_sampling.txt"
+orig_ebd <- paste0(ebd_data_folder, "ebd_rel", ebd_release, "/ebd_rel", ebd_release, ".txt")
+orig_sampling <- paste0(ebd_data_folder, "ebd_sampling_rel", ebd_release, "/ebd_sampling_rel", ebd_release, ".txt")
+
+# names of the processed ebird files
+f_ebd <- paste0(ebd_save, "ebd_", data_tag, ".txt")
+f_sampling <- paste0(ebd_save, "ebd_sampling_", data_tag, ".txt")
+
 if (!file.exists(f_ebd)) {
-  ebd_filtered <- auk_ebd("ebd_relAug-2018.txt", 
-                          file_sampling = "ebd_sampling_relAug-2018.txt") %>% 
+  ebd_filtered <- auk_ebd(orig_ebd, 
+                          file_sampling = orig_sampling) %>% 
     auk_species(c("Wood Thrush", "Northern Bobwhite", 
                   "White Ibis")) %>% 
     # southeastern coastal plain bcr
     auk_bcr(bcr = 27) %>% 
     # june, any year
-    auk_date(date = c("*-06-01", "*-06-30")) %>% 
+    auk_date(date = c("*-05-01", "*-06-30")) %>% 
     # exclude incidental and other non-standard protocols
     auk_protocol(protocol = c("Stationary", "Traveling")) %>% 
     auk_complete() %>% 
@@ -57,7 +71,8 @@ ebd_zf <- ebd_zf %>%
     duration_minutes <= 5 * 60,
     effort_distance_km <= 5,
     # last 10 years of data
-    year(observation_date) >= 2008,
+    year(observation_date) > 2016,
+    year(observation_date) < 2019,
     !is.na(time_observations_started),
     # 10 or fewer observers
     number_observers <= 10)
@@ -72,24 +87,26 @@ ebd_zf <- ebd_zf %>%
          observation_date, time_observations_started, 
          duration_minutes, effort_distance_km,
          number_observers)
-write_csv(ebd_zf, "data/ebd_june_bcr27_zf.csv", na = "")
+write_csv(ebd_zf, paste0(ebd_save, "ebd_", data_tag, "_zf.csv"), na = "")
 
 
 # bad data ----
 
 # data for bad practices
 # intentionally keep incomplete checklists and all protocols
-f_ebd_bad <- "data/ebd_june_bcr27_bad.txt"
-f_sampling_bad <- "data/ebd_june_bcr27_bad_sampling.txt"
+
+f_ebd_bad <- paste0(ebd_save, "ebd_", data_tag, "_bad.txt")
+f_sampling_bad <- paste0(ebd_save, "ebd_sampling_", data_tag, "_bad.txt")
+
 if (!file.exists(f_ebd_bad)) {
-  ebd_filtered <- auk_ebd("ebd_relAug-2018.txt", 
-                          file_sampling = "ebd_sampling_relAug-2018.txt") %>% 
+  ebd_filtered <- auk_ebd(orig_ebd, 
+                          file_sampling = orig_sampling) %>% 
     auk_species(c("Wood Thrush", "Northern Bobwhite", 
                   "White Ibis")) %>% 
     # southeastern coastal plain bcr
     auk_bcr(bcr = 27) %>% 
     # june, any year
-    auk_date(date = c("*-06-01", "*-06-30")) %>% 
+    auk_date(date = c("*-05-01", "*-06-30")) %>% 
     auk_filter(file = f_ebd_bad, file_sampling = f_sampling_bad)
 }
 # zero fill
@@ -114,7 +131,9 @@ ebd_zf_bad <- ebd_zf_bad %>%
 
 # additional filtering - last 10 years of data
 # additional effort filters intentionally not applied
-ebd_zf_bad <- filter(ebd_zf_bad, year(observation_date) >= 2008)
+ebd_zf_bad <- filter(ebd_zf_bad, 
+    year(observation_date) > 2016,
+    year(observation_date) < 2019)
 
 # output
 ebd_zf_bad <- ebd_zf_bad %>% 
@@ -126,13 +145,13 @@ ebd_zf_bad <- ebd_zf_bad %>%
          observation_date, time_observations_started, 
          duration_minutes, effort_distance_km,
          number_observers)
-write_csv(ebd_zf_bad, "data/ebd_june_bcr27_bad_zf.csv", na = "")
+write_csv(ebd_zf_bad, paste0(ebd_save, "ebd_", data_tag, "_zf_bad.csv"), na = "")
 
 
 # richness ----
 
 # all species data to calculate richness for cci
-f_all <- "data/ebd_june_bcr27_all-species.txt"
+f_all <- paste0(ebd_save, "ebd_", data_tag, "_zf_speciesrichness.csv")
 if (!file.exists(f_all)) {
   cols <- c("sampling_event_identifier", "group_identifier", "observer_id",
             "taxonomic_order", "scientific_name", "subspecies_scientific_name",
@@ -140,18 +159,20 @@ if (!file.exists(f_all)) {
             "observation_date", "time_observations_started",
             "duration_minutes", "effort_distance_km", "number_observers",
             "observation count")
-  auk_ebd("ebd_relAug-2018.txt") %>% 
+  auk_ebd(orig_ebd) %>% 
     # southeastern coastal plain bcr
     auk_bcr(bcr = 27) %>% 
     # june, any year
-    auk_date(date = c("*-06-01", "*-06-30")) %>% 
+    auk_date(date = c("*-05-01", "*-06-30")) %>% 
     # exclude incidental and other non-standard protocols
     auk_protocol(protocol = c("Stationary", "Traveling")) %>% 
     auk_complete() %>% 
     auk_filter(file = f_all, keep = cols)
+
+  ebd_all_zf <- auk_zerofill(f_all, f_sampling)
+  richness <- ebd_all_zf$observations %>% 
+    group_by(checklist_id) %>% 
+    summarize(n_species = sum(species_observed))
+  write_csv(richness, paste0(ebd_save, "ebd_", data_tag, "_zf_speciesrichness.csv"))
+
 }
-ebd_all_zf <- auk_zerofill(f_all, f_sampling)
-richness <- ebd_all_zf$observations %>% 
-  group_by(checklist_id) %>% 
-  summarize(n_species = sum(species_observed))
-write_csv(richness, "data/richness_june_bcr27.csv")
